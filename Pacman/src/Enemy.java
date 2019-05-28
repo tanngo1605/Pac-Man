9c63d693e8785053d403f74791447cd76dfa22a1
tanngo1605 <50272836+tanngo1605@users.noreply.github.com>
2019-05-28 22:30:15 +0700

    Add files via upload

--- Pacman/src/Enemy.java
+++ Pacman/src/Enemy.java
@@ -1,4 +1,32 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.util.Random;
 
-public class Enemy {
+public class Enemy extends Rectangle {
+	public int x;
+	public int y;
+	//dimension 800*640
+	Player player;
+	public Enemy(Player p)
+	{
+		this.player=p;
+		Random r = new Random();
+		int randomValueX = r.nextInt();
+		int randomValueY = r.nextInt();
+		setBounds (randomValueX,randomValueY,15,15);
+	
+	}
+
+	public void update (Graphics g)
+	{
+		g.drawRect(0, 0, 15, 15);
+		g.setColor(Color.red);
+		
+	}
+	public void move()
+	{
+		//find shortest way to player
+	}
 
 }
